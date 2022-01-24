4ad1f3000c8ad8acc912b1c1b700252ec7828b8b
Julia M <julialocked@gmail.com>
2022-01-24 20:41:34 +0100

    Nuevo comentario bueno

--- src/main/java/com/cds/ccle/management/luca/utils/LucaUtils.java
+++ src/main/java/com/cds/ccle/management/luca/utils/LucaUtils.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+//Importaciones nuevas
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 
